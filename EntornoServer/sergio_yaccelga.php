9d099a995638e23a5f032323cc0699fe868c15a5
ElMasterEstAqui <elfuturoestoday2@gmail.com>
2020-10-05 15:22:01 +0200

    no se

---
+++ EntornoServer/sergio_yaccelga.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>CV</title>
+    <style>
+        body{
+            background-color: aqua;
+        }
+    </style>
+</head>
+<body>
+<h1>Hola soy Sergio y estoy en segundo de DAW2 B)</h1>
+<?php
+
+$variable = 'hola ' . 'soy '. 'sergio';
+
+echo "<h1>" . $variable . "</h1>";
+?>
+</body>
+</html>
