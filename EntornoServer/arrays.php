b8955503df5ca6400972f926ef02593fa446f422
ElMasterEstAqui <elfuturoestoday2@gmail.com>
2020-10-15 15:14:21 +0200

    nada important

--- EntornoServer/arrays.php
+++ EntornoServer/arrays.php
@@ -13,12 +13,32 @@
         $arraisito[1] = "hola2";
         $arraisito[2] = "hola3";
         $arraisito[3] = "hola4";
-        print_r($arraisito);
+        $arraisito[5] = "hola5";
+        //print_r($arraisito);
 
-        for ($i=0; $i < count($arraisito); $i++) { 
-            echo "<br> $i .) ". $arraisito[$i];
+        $arraisito3 = array("nombre" => "paco","edad" => "23","idiomas" => array("ingles","fances","español"));
+        // for ($i=0; $i < count($arraisito); $i++) { 
+        //     echo "<br> $i .) ". $arraisito[$i];
+        // }
+        // foreach ($arraisito as $key => $value) {
+        //     echo "<br> $key .) $value";
+        // }
+        
+        // foreach ($arraisito as $value) {
+        //     echo "<br> $value";
+        // }
+        
+        foreach($arraisito3 as $key => $v){
+            if(is_array($v)){
+                foreach ($v as $va) {
+                    echo "<br> $va";
+                }
+            }else{
+                echo "$v";
+            }
+            echo "<br>";
         }
-    
+
     ?>
 </body>
 </html>
