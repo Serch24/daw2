1022858e3b9c518656045374c0544c73feeef1b1
Sergio <elfuturoestoday2@gmail.com>
2020-10-21 18:07:21 +0200

    .

---
+++ EntornoServer/Muestra.php
@@ -0,0 +1,30 @@
+﻿<!DOCTYPE html>
+<html lang="es">
+<head>
+<!---Escribir una página PHP utilizando las constantes predefinidas--->
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>Muestra</title>
+</head>
+<body>
+	<?php
+		$nombre='pepe';
+		$saludo='Buenos dias';
+		
+		//Buenos dias el $nombre es 'pepe'
+		echo $saludo  .' el $nombre es ' . '\'' . $nombre . '\'' . '<br>';
+		
+		echo "$saludo el \$nombre es  '$nombre'<br>";
+		
+		
+		
+		//Buenos dias el $nombre es "pepe"
+		echo $saludo . 'el $nombre es ' . '"' . $nombre . '"' . '<br>';
+		
+		echo "$saludo el \$nombre es ".  "\"$nombre\"" . '<br>';
+	
+	?>
+
+	
+</body>
+</html>
