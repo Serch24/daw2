0d89cdcabe6db1972ab63bc41349afb5f82d55f2
cuartoIntento <aaa@example.com>
2020-11-02 23:42:41 +0100

    js

--- EntornoCliente/dias_llovidos.js
+++ EntornoCliente/dias_llovidos.js
@@ -1,75 +1,62 @@
 'use strict'
-/* Recoger el número de mes, el día y los litros llovidos hasta que no se desee introducir más.
- Los meses y días podrán estar repetidos.
- Mostrar los nombres de los meses con el total de litros llovidos.
- Mostrar el día más lluvioso en la forma: "El día más lluvioso ha sido el 7 de Marzo".
- Mostrar el nombre de los meses ordenados descendentemente por número de litros llovidos.
-  Este apartado debe hacerse en primer lugar sin usar objetos (solo tablas) 
-  y en segundo lugar haciendo uso de objetos que contengan el nombre del mes y el total de litros. */
+/*  Recoger el número de mes, el día y los litros llovidos hasta que no se desee introducir más. Los meses y días podrán estar repetidos.
+    Mostrar los nombres de los meses con el total de litros llovidos.
+    Mostrar el día más lluvioso en la forma: "El día más lluvioso ha sido el 7 de Marzo".
+    Mostrar el nombre de los meses ordenados descendentemente por número de litros llovidos. Este apartado debe hacerse en primer lugar sin usar 
+    objetos (solo tablas) y en segundo lugar haciendo uso de objetos que contengan el nombre del mes y el total de litros. */
 
 
-var flag = false;
-var arrai = [];
-var sumatorio = 0;
-do {
-    arrai.push({mes: parseInt(prompt("Introduce el numero del mes.")),dia: parseInt(prompt("Introduce el dia.")), litros: parseInt(prompt("Introduce los litros llovidos.")),totales: 0 });
-    flag = confirm("¿desea continuar añadiendo?");
-} while (flag);
+   /*  var meses = [];
+    var diasylitros = [];
+    var flag = true;
+    var totalLitros = 0;
+    do{
+        meses.push(parseInt(prompt("dame el numero del mes")));
+        diasylitros.push([parseInt(prompt("dame el numero del dia")),parseInt(prompt("dame el numero de litros"))]);
+        flag = confirm("¿Desea continuar?");
+    }while(flag);
 
 
-for (let i = 0; i < arrai.length; i++) {
-    switch (arrai[i].mes) {
-        case 1: 
-            arrai[i].totales += arrai[i].litros;
-            break;
-        case 2:
-            arrai[i].totales += arrai[i].litros;
-            break;
-    
-        case 3:
-            arrai[i].totales += arrai[i].litros;
-            break;
+    console.log(diasylitros);
+    // debugger;
+    for (var i = 0; i < diasylitros.length; i++) {
+        for (var k = 0; k < diasylitros[i].length; k++) {      
+            
+            if (diasylitros[i][k]==meses[k+1]) {
+                totalLitros+= diasylitros[i][1];
+            }
+        }
+        console.log(`El mes de ${meses[i]} llovió ${totalLitros}`);
+    } */
 
-        case 4:
-            meses_totatel = {mes: "abril", total: sumatorio};
-            break;
 
-        case 5:
-            console.log("mayo");
-            break;
+var total = [];
+var meses = [];
+var f = [],c = [];
+var datos = [f][c];
 
-        case 6:
-            console.log("junio");
-            break;
-      
-        case 7:
-            console.log("julio");
-            break;
-        
-        case 8:
-            console.log("agosto");
-            break;
-    
-        case 9:
-            console.log("septiembre");
-            break;
-    
-        case 10:
-            console.log("octubre");
-            break;
+             for(var f=0;f<31;f++){//totalizamos la tabla para el 4 apartado
 
-        case 11:
-            console.log("noviembre");
-            break;
+                    total[f]=0;
+                    for(var c=0;c<12;c++){
+                        total[f]=datos[f][c];
+                    }
+             }
+             for(var f=0;f<31;f++){//Sacamos el mes con su total
+                 document.write(meses[f] + "" + total[f]);
+             }
 
-        case 12:
-            console.log("Diciembre");
-            break;
-
-        default:
-            console.log('nonono');
-            break;
-    }
-}
-
-console.log(arrai);
+             //con dos for y un if(datos[f][c]>mayor) averiguas 
+             for(var f=0;f<31;f++){
+                 for(var c=0;c<12;c++){
+                     if(datos[f][c]>mayor){
+                        mayor=datos[f][c];
+                        f.aux=f;
+                        c.aux=c;
+                     }
+               
+                 }
+           
+             }
+           
+             document.write(c.aux+1 +  meses[f.aux]);
