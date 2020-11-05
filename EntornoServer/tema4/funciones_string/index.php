ad203661051e84146e079cf08739bb66d09a7012
ElMasterEstAqui <elfuturoestoday2@gmail.com>
2020-11-05 15:23:18 +0100

    php

---
+++ EntornoServer/tema4/funciones_string/index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="author" content="Sergio Andrés Yaccelga Castillo">
+    <meta name="description" content="Tarea de formularios en php">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Login</title>
+</head>
+<body>
+    <form action="MENSAJES.php" method="get">
+    
+    <label for="user">User</label>
+    <input type="text" name="user" id="user">
+    <label for="pass">password</label>
+    <input type="password" name="pass" id="pass">
+    <input type="submit" value="enviar">
+    
+    </form>
+</body>
+</html>
