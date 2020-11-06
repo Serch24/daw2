d7c9ce70dfbb2b82d760c0dbd2011d30362e85e5
ElMasterEstAqui <elfuturoestoday2@gmail.com>
2020-11-06 11:42:41 +0100

    cosas de js

--- EntornoServer/tema4/cryp.php
+++ EntornoServer/tema4/cryp.php
@@ -1,14 +1,14 @@
 <!DOCTYPE html>
-<html lang="es">
-
+<html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>practicando</title>
+    <title>Document</title>
 </head>
-
 <body>
-    <script src="mathh.js"></script>
+    <?php
+        $a = crypt("sergio");
+        echo $a;
+    ?>
 </body>
-
 </html>
