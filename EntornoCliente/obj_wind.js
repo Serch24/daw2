6a237213d574acfb58b8fe7caa6fa13d217f6d38
wind_bug <you@example.com>
2020-11-13 14:22:40 +0100

    cosas varias

--- EntornoCliente/obj_wind.js
+++ EntornoCliente/obj_wind.js
@@ -1,6 +1,5 @@
 var b;
 function segundos(){
     var a = window.open('https://hub.docker.com/_/php/',"a","toolbar=yes,scrollbars=yes,resizable=yes,top=200,left=500,width=600,height=400");
-    clearInterval(b);
 }
-b = setInterval("segundos()",3000)
+segundos();
