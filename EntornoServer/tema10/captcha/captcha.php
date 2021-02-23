d0395d686332b0ec735606b269d8eb143533f688
serch <nope>
2021-02-23 23:14:53 +0100

    trate pero no pude :(

--- EntornoServer/tema10/captcha/captcha.php
+++ EntornoServer/tema10/captcha/captcha.php
@@ -1,25 +1,38 @@
 <?php
-$texto = "adiviname";
-$mifo = "./Noruega.png";
-$im = imagecreatefrompng($mifo);
-for ($i = 0; $i < strlen($texto); $i++) {
-        $letra = substr($texto, $i, 1);
-        $color = imagecolorallocate($im, 255, 255, 255);
-        $y = rand(90, 300);
-        $x = rand(90, 600);
-        $angulo = rand(0, 150);
-        imagettftext(
-                $im,
-                70,
-                $angulo,
-                $x,
-                $y,
-                $color,
-                "./CaskaydiaCove_Nerd_Font_Book.ttf",
-                $letra
-        );
+$crear_cookie = function ($texto) {
+        $mifo = "./mas.png";
+        $im = imagecreatefrompng($mifo);
+        for ($i = 0; $i < strlen($texto); $i++) {
+                $letra = substr($texto, $i, 1);
+                $y = rand(100, 555);
+                $x = $i + 40 + rand(100, 855);
+                $color = imagecolorallocate(
+                        $im,
+                        rand(0, 255),
+                        rand(0, 255),
+                        rand(0, 255)
+                );
+                $angulo = rand(0, 50);
+
+                imagettftext(
+                        $im,
+                        100,
+                        $angulo,
+                        $x,
+                        $y,
+                        $color,
+                        "./CaskaydiaCove_Nerd_Font_Book.ttf",
+                        $letra
+                );
+        }
+        header("Content-type: image/png");
+        header("Content-Disposition:inline; filename=nah.png");
+        imagepng($im);
+};
+
+if (isset($_GET["texto"])) {
+        $crear_cookie($_GET["texto"]);
+        setcookie("ready", 1);
 }
-header("Content-type: image/png");
-header("Content-Disposition:inline; filename=nah.png");
-imagepng($im);
+header("location:/");
 ?>
--- EntornoServer/tema10/captcha/index.php
+++ EntornoServer/tema10/captcha/index.php
@@ -6,12 +6,19 @@
         <title>captcha</title>
 </head>
 <body>
-        <form action="<?php echo $_SERVER["PHP_SELF"]; ?>" method="get">
+<?php if (isset($_COOKIE["ready"])) { ?>
+<form action="<?php echo $_SERVER["PHP_SELF"]; ?>" method="get">
                 <p>Introduzca código de seguridad</p>
                 <input type="text" name="texto" placeholder="escribelo">
                 <img src="captcha.php" alt="captcha" width="400" height="300"><br><br>
                 <button name="enviar">Enviar</button>
                 <button name="otro">Cargar otro captcha</button>
         </form>
+<?php } else {$texto = substr(
+                str_shuffle("0123456789abcdefghijklmnopqerstuvwxyz"),
+                0,
+                5
+        );
+        header("location:captcha.php?texto=$texto");} ?>
 </body>
 </html>
